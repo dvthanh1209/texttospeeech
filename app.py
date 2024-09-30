b0c24b30140cac16e8d141cb58ed9a9f674d94ea
Đinh Văn Thành <d.v.thanh1209@gmail.com>
2024-09-30 14:51:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-# Lấy API key từ biến môi trường
+
 api_key = os.environ.get('API_KEY')
 
 @app.route('/')
@@ -17,7 +17,7 @@ def convert():
     voice = request.form.get('voice', 'banmai')
     speed = request.form.get('speed', 0)
 
-    # Kiểm tra nếu API key chưa được đặt
+   
     if not api_key:
         return jsonify({"error": "API key chưa được cấu hình."}), 500
 
@@ -43,7 +43,7 @@ def convert():
     else:
         return jsonify({"error": "Lỗi khi gửi yêu cầu."}), response.status_code
 
-# Test route để kiểm tra ứng dụng Flask đang hoạt động
+
 @app.route('/test')
 def test():
     return "Hello, Flask is working!"
